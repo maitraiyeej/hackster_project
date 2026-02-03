ca82fbc9c4f5a8d959f103e7f326d8003e74fd39
Harsh Sharma <krish9515274@gmail.com>
2026-02-03 18:25:05 +0530

    Added protected route component

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -14,6 +14,7 @@ import UserProfile from "./pages/UserProfile";
 import ViewTeam from "./pages/ViewTeam";
 import { ToastContainer } from "react-toastify";
 import "react-toastify/dist/ReactToastify.css";
+import ProtectedRoute from "./components/ProtectedRoute";
 
 
 function App() {
@@ -32,20 +33,21 @@ function App() {
             <Route path="/login" element={<LoginPage />} />
             <Route path='/register' element={<RegisterPage />} />
 
-            <Route
-              path="/admin"
-              element={user && user.role === 'Admin' ? <AdminDashboard /> : <Navigate to='/' />}
-            />
-            <Route
-              path="/my-events"
-              element={user && user.role === 'Admin' ? <MyEvents /> : <Navigate to='/' />}
-            />
+
+            <Route element={<ProtectedRoute />}>
+              <Route path="/user/:id" element={<UserProfile />} />
+              <Route path="/hackathon/:id/create-team" element={<CreateTeam />} />
+              <Route path="/hackathon/:id/my-team" element={<ViewTeam />} />
+              <Route path="/manage-team/:teamId" element={<ManageTeam />} />
+            </Route>
+
+
+            <Route element={<ProtectedRoute adminOnly />}>
+              <Route path="/admin" element={<AdminDashboard />} />
+              <Route path="/my-events" element={<MyEvents />} />
+            </Route>
 
             <Route path="/hackathon/:id" element={<HackathonDetails />} />
-            <Route path="/hackathon/:id/create-team" element={<CreateTeam />} />
-            <Route path="/hackathon/:id/my-team" element={<ViewTeam />} />
-            <Route path="/manage-team/:teamId" element={<ManageTeam />} />
-            <Route path="/user/:id" element={<UserProfile />} />
 
             <Route path="*" element={<Navigate to="/" />} />
           </Routes>
